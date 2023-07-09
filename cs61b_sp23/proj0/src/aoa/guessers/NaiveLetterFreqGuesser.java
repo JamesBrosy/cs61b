5ac5da8e466d9b46ef55e2a0953f2a1898211fcf
JamesBrosy <1351786156@qq.com>
2023-07-09 22:58:57 +0800

    fixed proj0/src/guessers/NaiveLetterFreqGuesser.java

---
+++ cs61b_sp23/proj0/src/aoa/guessers/NaiveLetterFreqGuesser.java
@@ -0,0 +1,66 @@
+package aoa.guessers;
+
+import aoa.utils.FileUtils;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class NaiveLetterFreqGuesser implements Guesser {
+    private final List<String> words;
+
+    public NaiveLetterFreqGuesser(String dictionaryFile) {
+        words = FileUtils.readWords(dictionaryFile);
+    }
+
+    @Override
+    /** Makes a guess which ignores the given pattern. */
+    public char getGuess(String pattern, List<Character> guesses) {
+        return getGuess(guesses);
+    }
+
+    /** Returns a map from a given letter to its frequency across all words.
+     *  This task is similar to something you did in hw0b! */
+    public Map<Character, Integer> getFrequencyMap() {
+        Map<Character, Integer> freqMap = new TreeMap<>();
+        for (String item : words) {
+            for (int i = 0; i < item.length(); i++) {
+                char ch = item.charAt(i);
+                if (!freqMap.containsKey(ch)) {
+                    freqMap.put(ch, 1);
+                } else {
+                    freqMap.put(ch, freqMap.get(ch) + 1);
+                }
+            }
+        }
+        return freqMap;
+    }
+
+    /** Returns the most common letter in WORDS that has not yet been guessed
+     *  (and therefore isn't present in GUESSES). */
+    public char getGuess(List<Character> guesses) {
+        Map<Character, Integer> freqMap = getFrequencyMap();
+        if (freqMap.isEmpty()) {
+            return '?';
+        }
+        freqMap.put('?', 0);
+        char bestGuess = '?';
+        for (char item : freqMap.keySet()) {
+            if (guesses.contains(item)) {
+                continue;
+            }
+            if (freqMap.get(item) > freqMap.get(bestGuess)) {
+                    bestGuess = item;
+            }
+        }
+        return bestGuess;
+    }
+
+    public static void main(String[] args) {
+        NaiveLetterFreqGuesser nlfg = new NaiveLetterFreqGuesser("data/example.txt");
+        System.out.println("list of words: " + nlfg.words);
+        System.out.println("frequency map: " + nlfg.getFrequencyMap());
+
+        List<Character> guesses = List.of('e', 'l');
+        System.out.println("guess: " + nlfg.getGuess(guesses));
+    }
+}
