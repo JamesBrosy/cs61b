3dc99acbc1ff744a7f938ab4ff9d8a95061f8b27
JamesBrosy <1351786156@qq.com>
2023-07-14 22:28:26 +0800

    use helper in PatternAwareLetterFreqGuesser.java

--- cs61b_sp23/proj0/src/aoa/guessers/PatternAwareLetterFreqGuesser.java
+++ cs61b_sp23/proj0/src/aoa/guessers/PatternAwareLetterFreqGuesser.java
@@ -1,12 +1,9 @@
 package aoa.guessers;
 
+import aoa.helpers.HelperUtils;
 import aoa.utils.FileUtils;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
-import java.util.regex.*;
 
 public class PatternAwareLetterFreqGuesser implements Guesser {
     private final List<String> words;
@@ -15,52 +12,12 @@ public class PatternAwareLetterFreqGuesser implements Guesser {
         words = FileUtils.readWords(dictionaryFile);
     }
 
-    protected List<String> getMatchedWords(String pattern) {
-        List<String> matchedWords = new ArrayList<>();
-        String newPattern = pattern.replace('-', '.');
-         for (String item : words) {
-             if (Pattern.matches(newPattern, item)) {
-                 matchedWords.add(item);
-             }
-         }
-         return matchedWords;
-    }
-
-    protected Map<Character, Integer> getFrequentMap(String pattern) {
-        List<String> matchedWords = getMatchedWords(pattern);
-        Map<Character, Integer> freqMap = new TreeMap<>();
-        for (String item : matchedWords) {
-            for (int i = 0; i < item.length(); i++) {
-                char ch = item.charAt(i);
-                if (!freqMap.containsKey(ch)) {
-                    freqMap.put(ch, 1);
-                } else {
-                    freqMap.put(ch, freqMap.get(ch) + 1);
-                }
-            }
-        }
-        return freqMap;
-    }
-
     @Override
     /** Returns the most common letter in the set of valid words based on the current
      *  PATTERN. */
     public char getGuess(String pattern, List<Character> guesses) {
-        Map<Character, Integer> freqMap = getFrequentMap(pattern);
-        if (freqMap.isEmpty()) {
-            return '?';
-        }
-        freqMap.put('?', 0);
-        char bestGuess = '?';
-        for (char item : freqMap.keySet()) {
-            if (guesses.contains(item)) {
-                continue;
-            }
-            if (freqMap.get(item) > freqMap.get(bestGuess)) {
-                bestGuess = item;
-            }
-        }
-        return bestGuess;
+        String regex = pattern.replace('-', '.');
+        return HelperUtils.getGuess(regex, words, guesses);
     }
 
     public static void main(String[] args) {
