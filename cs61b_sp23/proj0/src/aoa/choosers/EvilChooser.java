83dc9ee3d78764b68477450bd56a0f72830a4282
JamesBrosy <1351786156@qq.com>
2023-07-15 15:20:22 +0800

    implements EvilChooser.java

--- cs61b_sp23/proj0/src/aoa/choosers/EvilChooser.java
+++ cs61b_sp23/proj0/src/aoa/choosers/EvilChooser.java
@@ -1,6 +1,8 @@
 package aoa.choosers;
 
-import java.util.List;
+import java.util.*;
+
+import aoa.helpers.HelperUtils;
 import edu.princeton.cs.algs4.StdRandom;
 import aoa.utils.FileUtils;
 
@@ -9,24 +11,48 @@ public class EvilChooser implements Chooser {
     private List<String> wordPool;
 
     public EvilChooser(int wordLength, String dictionaryFile) {
-        // TODO: Fill in this constructor.
+        if (wordLength < 1) {
+            throw new IllegalArgumentException("wordLength is less than one");
+        }
+        wordPool = FileUtils.readWordsOfLength(dictionaryFile, wordLength);
+        if (wordPool.isEmpty()) {
+            throw new IllegalStateException("No words found of wordsLength");
+        }
+
+        char[] tmp = new char[wordLength];
+        Arrays.fill(tmp, '-');
+        pattern = new String(tmp);
     }
 
     @Override
     public int makeGuess(char letter) {
-        // TODO: Fill in this method.
-        return 0;
+        Map<String, List<String>> patternToWordsMap = HelperUtils.generatePatternToWordsMap(wordPool, letter, pattern);
+        patternToWordsMap.put("?", List.of());
+        pattern = "?";
+        for (String item : patternToWordsMap.keySet()) {
+            if (patternToWordsMap.get(item).size() > patternToWordsMap.get(pattern).size()) {
+                pattern = item;
+            }
+        }
+        wordPool = patternToWordsMap.get(pattern);
+
+        int sum = 0;
+        for (int i = 0; i < pattern.length(); i++) {
+            if (letter == pattern.charAt(i)) {
+                sum++;
+            }
+        }
+        return sum;
     }
 
     @Override
     public String getPattern() {
-        // TODO: Fill in this method.
-        return "";
+        return pattern;
     }
 
     @Override
     public String getWord() {
-        // TODO: Fill in this method.
-        return "";
+        int randomLyChosenWordNumber = StdRandom.uniform(wordPool.size());
+        return wordPool.get(randomLyChosenWordNumber);
     }
 }
