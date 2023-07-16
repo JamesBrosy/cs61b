149843441b29fd42839871dc0aa681103d2451d6
JamesBrosy <1351786156@qq.com>
2023-07-16 23:19:47 +0800

    finished finding three passwords

--- cs61b_sp23/lab02/src/bomb/BombMain.java
+++ cs61b_sp23/lab02/src/bomb/BombMain.java
@@ -8,16 +8,15 @@ public class BombMain {
         if (args.length > 0) {
             phase = Integer.parseInt(args[0]);
         }
-        // TODO: Find the correct passwords to each phase using debugging techniques
         Bomb b = new Bomb();
         if (phase >= 0) {
-            b.phase0("Figure this out. I wonder where the phases are defined...");
+            b.phase0("39291226");
         }
         if (phase >= 1) {
-            b.phase1(null); // Figure this out too
+            b.phase1(IntList.of(0, 9, 3, 0, 8)); // Figure this out too
         }
         if (phase >= 2) {
-            b.phase2("Figure this out. I wonder where the phases are defined...");
+            b.phase2("0 ".repeat(1337) + "-81201430");
         }
     }
 }
