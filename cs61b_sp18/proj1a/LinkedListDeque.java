f75054a16b159606f8e6afb009f4a556c3802955
JamesBrosy <1351786156@qq.com>
2023-07-26 00:50:46 +0800

    finished LinkedListDeque

--- cs61b_sp18/proj1a/LinkedListDeque.java
+++ cs61b_sp18/proj1a/LinkedListDeque.java
@@ -1,53 +1,119 @@
 public class LinkedListDeque<T> {
 
+    private Node sentinel;
+
+    private int size;
+
+    private class Node {
+
+        private T data;
+
+        private Node prev;
+        private Node next;
+
+        Node() {
+            this.data = null;
+            this.prev = this;
+            this.next = this;
+        }
+
+        Node(T data, Node prev, Node next) {
+            this.data = data;
+            this.prev = prev;
+            this.next = next;
+        }
+    }
+
     /** Creates an empty linked list deque */
     public LinkedListDeque() {
-
+        this.sentinel = new Node();
+        this.size = 0;
     }
 
     /** Create a deep copy of other linked list deque */
     public LinkedListDeque(LinkedListDeque<T> other) {
-
+        sentinel =  new Node();
+        Node tmp = sentinel;
+        Node first = other.sentinel.next;
+        while (first != null) {
+            tmp.next = new Node(first.data, tmp, sentinel);
+            sentinel.prev = tmp.next;
+            tmp = tmp.next;
+            first = first.next;
+        }
+        size = other.size;
     }
 
     /** Adds an item of type {@code T} to the front of the deque */
     public void addFirst(T item) {
-
+        Node tmpFirst = sentinel.next;
+        tmpFirst.prev = new Node(item, sentinel, tmpFirst);
+        sentinel.next = tmpFirst.prev;
+        size++;
     }
 
     /** Adds an item of type {@code T} to the back of deque */
     public void addLast(T item) {
-
+        Node tmpLast = sentinel.prev;
+        tmpLast.next = new Node(item, sentinel.prev, sentinel);
+        sentinel.prev = tmpLast.next;
+        size++;
     }
 
     /** Returns true if deque is empty, false otherwise */
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     /** Returns the number of items in the deque */
     public int size() {
-        return 0;
+        return size;
     }
 
     /** Prints the items in the deque from first to last */
     public void printDeque() {
-
+        Node tmp = sentinel.next;
+        while (tmp != sentinel) {
+            System.out.print(tmp.data + " ");
+            tmp = tmp.next;
+        }
+        System.out.println();
     }
 
     /** Removes and returns the item at the front of the deque */
     public T removeFirst() {
-        return null;
+        if (size == 0) {
+            return null;
+        }
+        size--;
+        Node tmpFirst = sentinel.next;
+        sentinel.next = tmpFirst.next;
+        tmpFirst.next.prev = sentinel;
+        return tmpFirst.data;
     }
 
     /** Removes and returns the item of back of the deque */
     public T removeLast() {
-        return null;
+        if (size == 0) {
+            return null;
+        }
+        size--;
+        Node tmpLast = sentinel.prev;
+        sentinel.prev = tmpLast.prev;
+        tmpLast.prev.next = sentinel;
+        return tmpLast.data;
     }
 
     /** Gets the item at the given index */
     public T get(int index) {
-        return null;
+        if (index < 0 || index >= size) {
+            return null;
+        }
+        Node tmp = sentinel;
+        for (int i = 0; i < index + 1; i++) {
+            tmp = tmp.next;
+        }
+        return tmp.data;
     }
 
 }
