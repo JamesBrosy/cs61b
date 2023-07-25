ce62731dfc8c5e079dbd62a0acceaca766db8d7f
JamesBrosy <1351786156@qq.com>
2023-07-25 22:25:23 +0800

    cs61b-sp18 proj1a setup

---
+++ cs61b_sp18/proj1a/LinkedListDeque.java
@@ -0,0 +1,53 @@
+public class LinkedListDeque<T> {
+
+    /** Creates an empty linked list deque */
+    public LinkedListDeque() {
+
+    }
+
+    /** Create a deep copy of other linked list deque */
+    public LinkedListDeque(LinkedListDeque<T> other) {
+
+    }
+
+    /** Adds an item of type {@code T} to the front of the deque */
+    public void addFirst(T item) {
+
+    }
+
+    /** Adds an item of type {@code T} to the back of deque */
+    public void addLast(T item) {
+
+    }
+
+    /** Returns true if deque is empty, false otherwise */
+    public boolean isEmpty() {
+        return false;
+    }
+
+    /** Returns the number of items in the deque */
+    public int size() {
+        return 0;
+    }
+
+    /** Prints the items in the deque from first to last */
+    public void printDeque() {
+
+    }
+
+    /** Removes and returns the item at the front of the deque */
+    public T removeFirst() {
+        return null;
+    }
+
+    /** Removes and returns the item of back of the deque */
+    public T removeLast() {
+        return null;
+    }
+
+    /** Gets the item at the given index */
+    public T get(int index) {
+        return null;
+    }
+
+}
